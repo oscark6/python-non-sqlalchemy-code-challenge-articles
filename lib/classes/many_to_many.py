57eaa017d59a774a359a8348deb58c9ecc66bbdb
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-06-23 21:29:19 +0300

    still pushing

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,24 +1,61 @@
 class Article:
+    all_articles = []
+
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        Article.all_articles.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, new_author):
+        if isinstance(new_author, Author):
+            self._author = new_author
+        else:
+            raise ValueError("Author must be an instance of the Author class.")
+    
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if isinstance(new_magazine, Magazine):
+            self._magazine = new_magazine
+        else:
+            raise ValueError("Magazine must be an instance of the Magazine class.")
         
 class Author:
     def __init__(self, name):
-        self.name = name
-
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
     def articles(self):
-        pass
-
+        return [article for article in Article.all_articles if article.author == self]
+    
     def magazines(self):
-        pass
-
+        return list(set([article.magazine for article in self.articles()]))
+    
     def add_article(self, magazine, title):
-        pass
-
+        new_article = Article(self, magazine, title)
+        return new_article
+    
     def topic_areas(self):
-        pass
+        magazines = self.magazines()
+        if not magazines:
+            return None
+        return list(set([magazine.category for magazine in magazines]))
 
 class Magazine:
     def __init__(self, name, category):
